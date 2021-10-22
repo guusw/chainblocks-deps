084235e3fd773fc72a616abe00429e7538e0795c
Guus Waals <_@guusw.nl>
2021-10-22 15:48:31 +0200

    Initial commit

---
+++ checkout.sh
@@ -0,0 +1,33 @@
+git clone https://github.com/chainblocks/stb.git
+git clone https://github.com/chainblocks/json.git
+git clone https://github.com/chainblocks/bgfx.git
+git clone https://github.com/chainblocks/bx.git
+git clone https://github.com/chainblocks/bimg.git
+git clone https://github.com/chainblocks/imgui_club.git
+git clone https://github.com/chainblocks/kcp.git
+git clone https://github.com/chainblocks/magic_enum.git
+git clone https://github.com/chainblocks/context.git
+git clone https://github.com/chainblocks/cpp-taskflow.git
+git clone https://github.com/chainblocks/replxx.git
+git clone https://github.com/chainblocks/nameof.git
+git clone https://github.com/chainblocks/pdqsort.git
+git clone https://github.com/chainblocks/SDL-org.git SDL
+git clone https://github.com/chainblocks/implot.git
+git clone https://github.com/chainblocks/filesystem.git
+git clone https://github.com/chainblocks/libbacktrace.git
+git clone https://github.com/chainblocks/wasm3.git
+git clone https://github.com/chainblocks/xxHash.git
+git clone https://github.com/chainblocks/Catch2.git
+git clone https://github.com/chainblocks/linalg.git
+git clone https://github.com/chainblocks/tinygltf.git
+git clone https://github.com/chainblocks/miniaudio.git
+git clone https://github.com/chainblocks/utf8.h.git
+git clone https://github.com/chainblocks/spdlog.git
+git clone https://github.com/chainblocks/kissfft.git
+git clone https://github.com/chainblocks/pareto.git
+git clone https://github.com/chainblocks/ImGuizmo.git
+git clone https://github.com/chainblocks/bgfx.cmake.git
+# git clone https://github.com/chainblocks/brotli.git
+# git clone https://git.bakje.coffee/guus/brotli.git
+git clone ssh://coffee_git@git.bakje.coffee:2302/guus/brotli.git
+git clone https://github.com/chainblocks/snappy.git
